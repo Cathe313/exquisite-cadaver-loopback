d9f4bded88175468a40ead78decbff3d6550ae70
Catherine <ducharme.catherine@gmail.com>
2015-10-22 21:06:12 +0000

    Created two custom functions for the Stories and Lines tables

--- common/models/lines.js
+++ common/models/lines.js
@@ -1,3 +1,40 @@
-module.exports = function(Lines) {
+// var app = require('../../server/server');
 
+module.exports = function(Lines) {
+    Lines.newline = function(lineNumber, storyId, lineText, cb) {
+
+        var date = new Date();
+        // var Lines = app.models.Lines;
+
+        Lines.create( {"date": date, "storiesId": storyId, "lineText": lineText, "lineNumber": lineNumber}, function(err, obj){
+            if (!err) {
+                cb(null, obj);
+            }
+            else {
+                console.log('There as an ' + err);
+            }
+        });    
+
+    };
+    Lines.remoteMethod(
+        'newline', {
+            accepts: [{
+                arg: 'lineNumber',
+                type: 'number',
+                required: true
+            }, {
+                arg: 'storyId',
+                type: 'number',
+                required: true
+            }, {
+                arg: 'lineText',
+                type: 'string',
+                required: true
+            }],
+            returns: {
+                arg: 'response',
+                type: 'string'
+            }
+        }
+    );
 };
--- common/models/stories.js
+++ common/models/stories.js
@@ -1,3 +1,44 @@
-module.exports = function(Stories) {
+var app = require('../../server/server');
 
+module.exports = function(Stories) {
+    Stories.newstory = function(length, lineText, cb) {
+
+        var date = new Date();
+        var Lines = app.models.Lines;
+        
+        Stories.create( {"date": date, "rating":  0, "incomplete": true, "length": length}, function(err, storyObj) {
+            if (!err) {
+                Lines.create( {"date": date, "storiesId": storyObj.id, "lineText": lineText, "lineNumber": 1}, function(err, obj){
+                    if (!err) {
+                        cb(null, storyObj);
+                    }
+                    else {
+                        console.log('There as an ' + err + 'in the Lines function');
+                    }
+                });    
+            }
+            else {
+                console.log('There as an ' + err + 'in the Stories function');
+            }
+            
+            
+        });
+    };
+    Stories.remoteMethod(
+        'newstory', {
+            accepts: [{
+                arg: 'length',
+                type: 'number',
+                required: true
+            }, {
+                arg: 'lineText',
+                type: 'string',
+                required: true
+            }],
+            returns: {
+                arg: 'response',
+                type: 'string'
+            }
+        }
+    );
 };
